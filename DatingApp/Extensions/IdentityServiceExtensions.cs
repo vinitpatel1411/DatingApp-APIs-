018cf4552a811749c26a7665ff091a4d971b8cd9
Vinit Patel <vinit1411@gmail.com>
2023-10-16 01:43:50 +0530

    04 - Authentication basics

--- DatingApp/Extensions/IdentityServiceExtensions.cs
+++ DatingApp/Extensions/IdentityServiceExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DatingApp.Extensions
@@ -24,7 +25,7 @@ namespace DatingApp.Extensions
                 };
             });
 
-            return services
+            return services;
         }
     }
 }
